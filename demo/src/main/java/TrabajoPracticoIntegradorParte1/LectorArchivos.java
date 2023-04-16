5debeecc4caf964781844f11bcdef5b94a2d3fe4
Nahuel Agustin Dumo <nahueldumo2014@gmail.com>
2023-04-16 13:53:41 -0300

    se realizo la 3 parte del TPI que era la conexion a una base de datos la cual contiene todos los datos de los partidos Y los pronosticos se leen por archivo csv

--- demo/src/main/java/TrabajoPracticoIntegradorParte1/LectorArchivos.java
+++ demo/src/main/java/TrabajoPracticoIntegradorParte1/LectorArchivos.java
@@ -12,7 +12,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
 public class LectorArchivos {
 
     private static ArrayList<Ronda> rondas = new ArrayList<Ronda>();
